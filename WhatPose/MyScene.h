e705f1a9a03b6986296ad7b4c6804f9fce92679e
liyan <707704996@qq.com>
2014-06-05 17:25:37 +0800

    第一次提交

---
+++ WhatPose/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  WhatPose
+//
+
+//  Copyright (c) 2014年 李言. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ WhatPose/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WhatPose
+//
+
+//  Copyright (c) 2014年 李言. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
